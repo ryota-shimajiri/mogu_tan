a25fe1341f4a8090e49e727512ae2b45fd59fe52
shimajiri <ryota04130413@gmail.com>
2022-05-09 10:42:58 +0900

    update

--- mogu_tan.js
+++ mogu_tan.js
@@ -8,15 +8,7 @@ window.onload = function() {
 
 function start() {
 
-    mogus.push(document.getElementById("image_mogu1"));
-    mogus.push(document.getElementById("image_mogu2"));
-    mogus.push(document.getElementById("image_mogu3"));
-    mogus.push(document.getElementById("image_mogu4"));
-    mogus.push(document.getElementById("image_mogu5"));
-    mogus.push(document.getElementById("image_mogu6"));
-    mogus.push(document.getElementById("image_mogu7"));
-    mogus.push(document.getElementById("image_mogu8"));
-    mogus.push(document.getElementById("image_mogu9"));
+    mogus.push(document.getElementById(""));
     
     mogus.forEach(mogu => {
         onMogutan(mogu);
@@ -32,23 +24,15 @@ function onMogutan(mogu) {
     mogu.onclick = () => {
         // パス最後の/を取り除き、画像名を取得する
         const fileName = mogu.src.split('/').pop();
-        if (fileName === "mogu_tan2.PNG") {
-            mogu.src = "img/mogu_tan3.PNG";
-            count ++;
-            countMogu = document.getElementById("countMogu");
-            countMogu.innerHTML = count;
-        }
     };
 }
 
 // モグタンが隠れる処理
-function hiddenMogutan(mogu) {
-    mogu.src = "img/mogu_tan1.PNG";
-    setTimeout(() => visibleMogutan(mogu), Math.random() * 5 * 1000);
+function hiddenMogutan() {
 }
 
 // モグタンが現れる処理
-function visibleMogutan(mogu) {
+function visibleMogutan() {
     mogu.src = "img/mogu_tan2.PNG";
     setTimeout(() => hiddenMogutan(mogu), Math.random() * 5 * 1000);
 }
---
+++ sample.js
@@ -0,0 +1,54 @@
+let count = 0;
+let countMogu;
+let mogus = [];
+
+window.onload = function() {
+    start();
+}
+
+function start() {
+
+    mogus.push(document.getElementById("image_mogu1"));
+    mogus.push(document.getElementById("image_mogu2"));
+    mogus.push(document.getElementById("image_mogu3"));
+    mogus.push(document.getElementById("image_mogu4"));
+    mogus.push(document.getElementById("image_mogu5"));
+    mogus.push(document.getElementById("image_mogu6"));
+    mogus.push(document.getElementById("image_mogu7"));
+    mogus.push(document.getElementById("image_mogu8"));
+    mogus.push(document.getElementById("image_mogu9"));
+    
+    mogus.forEach(mogu => {
+        onMogutan(mogu);
+        moveMogtan(mogu);
+    });
+}
+
+function moveMogtan(mogu) {
+    setTimeout(() => hiddenMogutan(mogu), Math.random() * 5 * 1000);
+}
+
+function onMogutan(mogu) {
+    mogu.onclick = () => {
+        // パス最後の/を取り除き、画像名を取得する
+        const fileName = mogu.src.split('/').pop();
+        if (fileName === "mogu_tan2.PNG") {
+            mogu.src = "img/mogu_tan3.PNG";
+            count ++;
+            countMogu = document.getElementById("countMogu");
+            countMogu.innerHTML = count;
+        }
+    };
+}
+
+// モグタンが隠れる処理
+function hiddenMogutan(mogu) {
+    mogu.src = "img/mogu_tan1.PNG";
+    setTimeout(() => visibleMogutan(mogu), Math.random() * 5 * 1000);
+}
+
+// モグタンが現れる処理
+function visibleMogutan(mogu) {
+    mogu.src = "img/mogu_tan2.PNG";
+    setTimeout(() => hiddenMogutan(mogu), Math.random() * 5 * 1000);
+}
